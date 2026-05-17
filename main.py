513e5a9a2fe6dd4c3502d336844d039af27cabda
Dan Sakamoto <contact@dansakamoto.com>
2026-05-17 00:48:57 -0400

    import work from previous draft

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from astroquery.simbad import Simbad
+
+
 def main():
-    print("Hello from parallax!")
+    simbad = Simbad()
+    simbad.add_votable_fields("otype", "alltypes", "dim")
+    info_simbad = simbad.query_object("NGC4038")
+    print(info_simbad["main_id"][0])
 
 
 if __name__ == "__main__":
