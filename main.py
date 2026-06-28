7a7112434745a054da8d174bb7e8bf28ee01e08a
Dan Sakamoto <contact@dansakamoto.com>
2026-06-28 02:44:35 -0400

    retrieve star list and convert to json

--- main.py
+++ main.py
@@ -1,11 +1,50 @@
 from astroquery.simbad import Simbad
+import numpy as np
+import json
 
 
 def main():
     simbad = Simbad()
-    simbad.add_votable_fields("otype", "alltypes", "dim")
-    info_simbad = simbad.query_object("NGC4038")
-    print(info_simbad["main_id"][0])
+
+    simbad.add_votable_fields(
+        "otype", "mesDistance", "plx_value", "plx_qual", "plx_err", "plx_err_prec"
+    )
+
+    info_simbad = simbad.query_object(
+        "* Lib", wildcard=True, criteria="otype = 'star..'"
+    )
+
+    data_raw = info_simbad.pformat()
+    data_formatted = {}
+
+    for row in info_simbad:
+        if row["main_id"] in data_formatted:
+            continue
+
+        data = {
+            "ra": row["ra"],
+            "dec": row["dec"],
+            "otype": row["otype"],
+        }
+
+        if row["plx_value"] is not np.ma.masked:
+            data["plx_value"] = row["plx_value"]
+
+        if row["mesdistance.dist"] is not np.ma.masked:
+            data["dist"] = row["mesdistance.dist"]
+            data["dist_unit"] = row["mesdistance.unit"].strip()
+            data["dist_method"] = row["mesdistance.method"].strip()
+
+        data_formatted[row["main_id"]] = data
+
+    fName = f"data/results.json"
+    with open(fName, "w") as f:
+        print("\nWriting results to file " + fName)
+        f.write(json.dumps(data_formatted, indent=2, ensure_ascii=False))
+        f.close()
+
+
+#    np.savetxt("data/results.csv", info_simbad.pformat(), delimiter=",", fmt="%s")
 
 
 if __name__ == "__main__":
