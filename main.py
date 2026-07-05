6f76921d07405eacb9a74f501213a1822e3854e2
Dan Sakamoto <contact@dansakamoto.com>
2026-07-05 04:55:59 -0400

    async requests and informational homepage

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from astroquery.simbad import Simbad
 import numpy as np
 from fastapi import FastAPI
-from time import sleep
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+import asyncio
 
 app = FastAPI()
+
+app.mount("/style", StaticFiles(directory="static"), name="static")
 
 constellations = {
     "orion": "* Ori",
@@ -24,13 +28,49 @@ constellations = {
 }
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"status": "running", "supported_keys": list(constellations.keys())}
+    links = ""
+    for c in constellations:
+        links += '<li><a href="' + c + '">' + c + "</a></li>"
+
+    return (
+        """
+    <html>
+        <head>
+            <title>Constellations</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+            <link rel="stylesheet" type="text/css" href="style/main.css">
+        </head>
+        <body>
+            <div class="header">
+            <h1>Constellations API</h1>
+            <p>Returns a JSON-formatted list of position data for all stars within a constellation - from <a href="https://simbad.cds.unistra.fr/simbad/">SIMBAD</a>
+            </div>
+            <div class="content">
+            <p>supported keys:</p>
+            <ul>
+                """
+        + links
+        + """
+            </u>
+            </div>
+        </body>
+    </html>
+    """
+    )
 
 
 @app.get("/{item_key}")
-def read_item(item_key: str):
+async def get_data(item_key: str):
+    loop = asyncio.get_running_loop()
+    res = await loop.run_in_executor(None, call_SIMBAD, item_key)
+    return res
+
+
+def call_SIMBAD(item_key: str):
     SELECTED = item_key
 
     if SELECTED.lower() not in constellations:
@@ -48,7 +88,6 @@ def read_item(item_key: str):
         STAR_CODE, wildcard=True, criteria="otype = 'star..'", async_job=True
     )
 
-    data_raw = info_simbad.pformat()
     data_formatted = {}
 
     for row in info_simbad:
