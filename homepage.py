b5d1fe394cd39a532092958dc3ef80d2fad96609
Dan Sakamoto <contact@dansakamoto.com>
2026-07-29 22:55:36 -0400

    fix UI element locations on large display

--- homepage.py
+++ homepage.py
@@ -82,6 +82,7 @@ class Homepage:
                         margin: 0;
                         display: flex;
                         flex-direction: column;
+                        min-height: 100vh;
                         }
 
                         div.header {
